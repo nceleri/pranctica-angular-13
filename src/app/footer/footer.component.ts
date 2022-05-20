344dd3a75ecb8bfea283680b385506b8012e5c6e
nceleri <nicoceleri@gmail.com>
2022-05-20 12:19:36 -0300

    arreglo del footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  public autor : any = {nombre: 'Nicolas', apellido: 'Celeri'};
+
   constructor() { }
 
   ngOnInit(): void {
